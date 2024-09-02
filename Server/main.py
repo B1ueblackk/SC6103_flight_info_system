d4dbb1eac6419573cb263c3a4df00d9a1e2700b3
B1ueblackk <191250195@smail.nju.edu.cn>
2024-09-02 17:02:30 +0800

    feat server: reserve_seats/monitor_updates

--- Server/main.py
+++ Server/main.py
@@ -5,13 +5,6 @@ from Server.server import Server
 if __name__ == '__main__':
     try:
         server = Server()
-        server.add_flight(
-            flight_identifier=102,
-            source_place="Beijing",
-            destination_place="Los Angeles",
-            departure_time=datetime(2024, 11, 1, 19, 30),
-            airfare=299.99,
-            seat_availability=56)
         server.start_listening()
     except Exception as e:
         print(str(e))
--- Server/server.py
+++ Server/server.py
@@ -1,6 +1,6 @@
 import json
 import socket
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from pymongo import MongoClient
 from Server.utils.data_process import binary_string_to_string
@@ -16,7 +16,8 @@ class Server:
         self.port = int(config.get('port', 12345))
         print(f"Server ip: {self.host}:{self.port}")
         self.server_socket = None
-
+        self.client_address = None
+        self.monitor_dict = {}
         # 从配置文件中获取MongoDB连接参数
         host = config['mongodb']['host']
         port = config['mongodb']['port']
@@ -47,20 +48,22 @@ class Server:
         try:
             while True:
                 # 等待接收数据，1024为缓冲区大小
-                data, client_address = self.server_socket.recvfrom(1024)
+                data, self.client_address = self.server_socket.recvfrom(1024)
                 text = binary_string_to_string(data.decode('utf-8'))
-                print(f"接收到来自 {client_address} 的数据: {text}")
+                print(f"接收到来自 {self.client_address} 的数据: {text}")
                 ret_flag, ret_msg = self.handle_request(binary_string_to_string(data.decode('utf-8')))
+                print(ret_msg)
                 if ret_flag == 0:
+                    # todo 转为01串
                     response = f"{ret_msg}"
-                    self.server_socket.sendto(response.encode('utf-8'), client_address)
+                    self.server_socket.sendto(response.encode('utf-8'), self.client_address)
         except KeyboardInterrupt:
             self.server_socket.close()
             print("服务器已关闭")
         except Exception as e:
             print(str(e))
 
-    def handle_request(self, data: str) -> str:
+    def handle_request(self, data: str) -> (int, str):
         opt = data.split(';')[0]
         # 使用 getattr 从对象 obj 中获取方法
         method = getattr(self, opt, data)
@@ -85,20 +88,113 @@ class Server:
         self.collection.insert_one(flight_document)
         self.collection.create_index([("flight_identifier", 1)], unique=True)
 
-
+    # 返回标志位(0:success, 1:fail) + 所有符合条件的航班信息/错误信息
     def query_flight(self, data: str) -> (int, str):
         try:
             source_place = data.split(';')[1]
             destination_place = data.split(';')[2]
             ret = []
-            for flight in self.collection:
-                if flight.source_place == source_place and flight.destination_place == destination_place:
-                    ret.append(flight)
+            query = {
+                "source_place": source_place,
+                "destination_place": destination_place
+            }
+            projection = {
+                "_id": 0
+            }
+            # 执行查询并应用投影
+            cursor = self.collection.find(query, projection)
+            # 将所有匹配的航班信息添加到返回列表中
+            ret = [flight for flight in cursor]
             if len(ret) == 0:
                 return 0, "No flights matched!"
             return 0, ''.join([flight.__repr__() for flight in ret])
         except Exception as e:
             return 1, str(e)
+
+    # 返回标志位(0:success, 1:fail) + 对应航班信息/错误信息
+    def query_flight_info(self, data: str) -> (int, str):
+        try:
+            flight_identifier = int(data.split(';')[1])
+            flight_info = self.collection.find_one(
+                {
+                    "flight_identifier": flight_identifier
+                },
+                projection = {
+                    "_id": 0
+                }
+            )
+            return 0, flight_info
+        except Exception as e:
+            return 1, str(e)
+
+    def reserve_seats(self, data: str) -> (int, str):
+        try:
+            # 解析传入的数据
+            flight_identifier = int(data.split(';')[1])
+            seats_count = int(data.split(';')[2])
+
+            # 查找航班信息
+            flight_info = self.collection.find_one({"flight_identifier": flight_identifier})
+            # 检查座位数量是否足够
+            if flight_info["seat_availability"] < seats_count:
+                return 0, "Seats not enough!"
+            else:
+                # 更新座位数量
+                new_seat_availability = flight_info["seat_availability"] - seats_count
+                self.collection.update_one(
+                    {"flight_identifier": flight_identifier},
+                    {"$set": {"seat_availability": new_seat_availability}}
+                )
+                self.reserve_seats_callback(flight_identifier, new_seat_availability)
+                return 0, "Seats reserved!"
+        except Exception as e:
+            return 1, str(e)
+
+    def reserve_seats_callback(self, flight_identifier, new_seat_availability):
+        if flight_identifier in self.monitor_dict:
+            monitor_list = self.monitor_dict[flight_identifier]
+            for monitor in monitor_list:
+                if datetime.now() < monitor["end_time"]:
+                    # todo 转为01串
+                    response = f"seats' availability updates: {new_seat_availability}"
+                    self.server_socket.sendto(response.encode('utf-8'), monitor["client_address"])
+
+    def cleanup_expired_monitors(self):
+        current_time = datetime.now()
+        for flight_identifier in list(self.monitor_dict.keys()):
+            # 过滤掉结束时间小于当前时间的监听记录
+            self.monitor_dict[flight_identifier] = [
+                monitor for monitor in self.monitor_dict[flight_identifier]
+                if monitor["end_time"] > current_time
+            ]
+            # 如果航班号的监听列表为空，删除这个航班号的记录
+            if not self.monitor_dict[flight_identifier]:
+                del self.monitor_dict[flight_identifier]
+
+    def monitor_update(self, data: str) -> (int, str):
+        try:
+            # 解析输入数据
+            flight_identifier = int(data.split(';')[1])
+            monitor_period = int(data.split(';')[2])
+
+            # 计算监听的结束时间
+            current_time = datetime.now()
+            end_time = current_time + timedelta(minutes=monitor_period)
+
+            # 更新监听字典
+            if flight_identifier not in self.monitor_dict:
+                self.monitor_dict[flight_identifier] = []
+
+            # 添加用户监听请求，包括监听的结束时间
+            self.monitor_dict[flight_identifier].append({
+                "client_address": self.client_address,  # 假设 client_address 是在其他地方设置的
+                "end_time": end_time
+            })
+            return 0, "Monitoring started!"
+        except Exception as e:
+            return 1, str(e)
+
+
 
 if __name__ == "__main__":
     print(1)
