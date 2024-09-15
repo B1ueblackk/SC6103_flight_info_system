8c2495e4df73ca733516d6687b96ea01f2134b8a
B1ueblackk <191250195@smail.nju.edu.cn>
2024-09-15 15:53:43 +0800

    build

--- Client/app.py
+++ Client/app.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 from flask import Flask, request, jsonify, render_template, session, redirect, url_for
@@ -262,4 +263,6 @@ def query_all_orders():
 if __name__ == "__main__":
     print("Client start.", file=sys.stderr)
     # socketio.run(app, host='127.0.0.1', port=5000, debug=True, allow_unsafe_werkzeug=True)
-    socketio.run(app, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get('PORT', 5000))  # 使用环境变量 PORT
+    app.run()
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
